4633eb461beebe68d13c289d6e9e57197983f76a
Wojciech Antos <wojciech.antos@teems.it>
2024-06-05 07:58:06 +0200

    Customers page implemented

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,25 @@
 import { Metadata } from 'next';
+import { Suspense } from 'react';
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { TableRowSkeleton } from '@/app/ui/skeletons';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { query?: string };
+}) {
+  const query = searchParams?.query || '';
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <Suspense fallback={<TableRowSkeleton />}>
+      <Table customers={customers} />
+    </Suspense>
+  );
 }
